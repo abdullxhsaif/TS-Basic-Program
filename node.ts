4cdc420630c9c6c1a01a15c86ad56b7365caf2ae
abdullxhsaif <abdullahsaifpriv@gmail.com>
2025-09-01 22:08:52 +0500

    save changes

---
+++ node.ts
@@ -0,0 +1,15 @@
+type person = {
+    name : string,
+    age : number,
+};
+
+function greet(person : person):string {
+    return `hello ${person.name}! you are ${person.age} years old.`;
+}
+
+const user : person = {
+    name : "saif",
+    age : 25
+};
+
+console.log(greet(user));
